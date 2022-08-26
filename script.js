2f3271d30fbaf0a88ef7804a24b348bd2ebca96e
Davide Passerò <shardonian86@gmail.com>
2022-08-26 17:14:50 +0200

    Style and functionality: Get the user choice

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const buttonRules = document.querySelector('#btn-rules');
 const rules = document.querySelector('#rules');
 const closeRules = document.querySelector('.close-rules');
+let gameContainer = document.querySelector('.triangle-bg-container');
+let choices = document.querySelector('.choices');
 // The Rules modal
 buttonRules.onclick = function () {
     rules.style.display = "block";
@@ -9,3 +11,21 @@ buttonRules.onclick = function () {
 closeRules.onclick = function () {
     rules.style.display = "none";
 };
+const choiceOption = {
+    "paper": "./images/icon-paper.svg",
+    "scissors": "./images/icon-scissors.svg",
+    "rock": "./images/icon-rock.svg"
+};
+// Hide the game response and "play again" button!
+function hideResponse() {
+    choices.style.display = "none";
+}
+;
+hideResponse();
+// The function si called by the onClick event in HTML file!
+function getUserChoice(choice) {
+    gameContainer.style.display = "none";
+    choices.style.display = "grid";
+    document.getElementById('userChoiceImg').src = choiceOption[choice];
+}
+;
--- script.ts
+++ script.ts
@@ -2,6 +2,9 @@ const buttonRules = document.querySelector('#btn-rules') as  HTMLButtonElement;
 const rules = document.querySelector('#rules') as  HTMLDivElement;
 const closeRules = document.querySelector('.close-rules') as  HTMLImageElement;
 
+let gameContainer = document.querySelector('.triangle-bg-container') as HTMLDivElement;
+let choices = document.querySelector('.choices') as  HTMLDivElement;
+
 // The Rules modal
 buttonRules.onclick = function() {
     rules.style.display = "block";
@@ -10,3 +13,32 @@ buttonRules.onclick = function() {
 closeRules.onclick = function() {
     rules.style.display = "none";
   };
+
+// Images paths are stored in an object
+// so that i could better solve the problem of changing the src image!
+interface choiceOption {
+  "paper":string;
+  "scissors":string;
+  "rock":string;
+}
+
+const choiceOption = {
+  "paper": "./images/icon-paper.svg",
+  "scissors": "./images/icon-scissors.svg",
+  "rock": "./images/icon-rock.svg"
+};
+
+// Hide the game response and "play again" button!
+function hideResponse(): void {
+  choices.style.display = "none";
+};
+hideResponse();
+
+// The function si called by the onClick event in HTML file!
+function getUserChoice(choice:string) {
+
+  gameContainer.style.display = "none";
+  choices.style.display = "grid";
+
+ (document.getElementById('userChoiceImg') as HTMLImageElement).src = choiceOption[choice];
+};
